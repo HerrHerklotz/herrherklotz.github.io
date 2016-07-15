03dfb8d63b2cc8c41589d97ce0d7625558c2702a
HerrHerklotz <herrherklotz@gmail.com>
2016-07-16 01:08:54 +0200

    update

--- controller.js
+++ controller.js
@@ -5,15 +5,21 @@ class AppController {
   constructor($document, $rootScope, $scope, $location, $mdSidenav) {
     let self = this;
     this.$document = $document;
+    this.$rootScope = $rootScope;
     this.$scope = $scope;
     this.$location = $location;
     this.$mdSidenav = $mdSidenav;
+
+    
 
     this.$scope.desktop = true;
     if ($location.path().endsWith('app'))
     {
       this.$scope.desktop = false;
     }
+
+    this.$rootScope.selectedPage = 'blog';
+    this.$scope.linkAPI = {'text-color':'#F00'};
   }
 
   setPage(oName) {
